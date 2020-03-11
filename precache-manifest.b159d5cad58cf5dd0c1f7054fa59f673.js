74d24015d31aa1070c257b8b9e3d61aa6b79b3a6
wojtekxjavascript <wojciech.bronsztejn@gmail.com>
2020-03-11 18:12:39 +0100

    Updates

---
+++ precache-manifest.b159d5cad58cf5dd0c1f7054fa59f673.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "313fa47d80585d26d414ac655ef1d155",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3ba2bfe7109199946a0a",
+    "url": "/monsters-rolodex/static/css/main.862bea81.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ba2bfe7109199946a0a",
+    "url": "/monsters-rolodex/static/js/main.42db21ff.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
